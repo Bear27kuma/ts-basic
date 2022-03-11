e32ebd92633e96fc7b00b19b6563abbc777e0b23
Yuta Okuma <y.dream.nest@icloud.com>
2022-03-11 20:23:30 +0900

    特殊なvoid型とnever型

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,17 @@
 // world.sayHello(root);
 
 // 03.基本の型定義
-import { unknownSample, anySample, notExistSample, primitiveSample } from './basic';
+// import { unknownSample, anySample, notExistSample, primitiveSample } from './basic';
+//
+// unknownSample();
+// anySample();
+// notExistSample();
+// primitiveSample();
 
-unknownSample();
-anySample();
-notExistSample();
-primitiveSample();
+// 04.関数の型定義
+import { logMessage, logMessage2, logMessage3, logMessage4 } from './function/basic';
+
+logMessage('Hello TypeScript!');
+logMessage2('Hello TypeScript!');
+logMessage3('Hello TypeScript!');
+logMessage4('Hello TypeScript!');
