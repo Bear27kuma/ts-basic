fd0711f307988ed44f73151800e080d494b53c56
Yuta Okuma <y.dream.nest@icloud.com>
2022-03-11 22:50:14 +0900

    オブジェクトリテラル記法で型定義

--- src/index.ts
+++ src/index.ts
@@ -15,20 +15,25 @@
 // primitiveSample();
 
 // 04.関数の型定義
-import { logMessage, logMessage2, logMessage3, logMessage4 } from './function/basic';
-import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters';
-import { logMessage5, logMessage6 } from './function/basic';
+// import { logMessage, logMessage2, logMessage3, logMessage4 } from './function/basic';
+// import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters';
+// import { logMessage5, logMessage6 } from './function/basic';
+//
+// logMessage('Hello TypeScript!');
+// logMessage2('Hello TypeScript!');
+// logMessage3('Hello TypeScript!');
+// logMessage4('Hello TypeScript!');
+//
+// isUserSignedIn('ABC', 'Kumahack');
+// isUserSignedIn('DEF');
+// isUserSignedIn2('ABC');
+// const sum = sumProductsPrice(100, 200, 300, 400, 500);
+// console.log('Function parameters sample 5:', sum);
+//
+// logMessage5('LogMessage');
+// logMessage6('FullLogMessage');
 
-logMessage('Hello TypeScript!');
-logMessage2('Hello TypeScript!');
-logMessage3('Hello TypeScript!');
-logMessage4('Hello TypeScript!');
+// 05.オブジェクトの型定義
+import objectSample from './object/object';
 
-isUserSignedIn('ABC', 'Kumahack');
-isUserSignedIn('DEF');
-isUserSignedIn2('ABC');
-const sum = sumProductsPrice(100, 200, 300, 400, 500);
-console.log('Function parameters sample 5:', sum);
-
-logMessage5('LogMessage');
-logMessage6('FullLogMessage');
+objectSample();
---
+++ src/object/object.ts
@@ -0,0 +1,24 @@
+export default function objectSample() {
+  const a: object = {
+    name: 'Kumahack',
+    age: 26,
+  };
+  // a.name;
+
+  // オブジェクトリテラル記法で型定義
+  let country: {
+    language: string;
+    name: string;
+  } = {
+    language: 'Japanese',
+    name: 'Japan',
+  };
+
+  console.log('Object object sample 1:', country);
+
+  country = {
+    language: 'English',
+    name: 'United States of America',
+  };
+  console.log('Object object sample 2:', country);
+}
