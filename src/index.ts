3b500e55707e36c8fe25acf4d723877b0ca06200
Yuta Okuma <y.dream.nest@icloud.com>
2022-03-11 21:18:00 +0900

    オプションパラメーターとデフォルトパラメーター

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,13 @@
 
 // 04.関数の型定義
 import { logMessage, logMessage2, logMessage3, logMessage4 } from './function/basic';
+import { isUserSignedIn, isUserSignedIn2 } from './function/parameters';
 
 logMessage('Hello TypeScript!');
 logMessage2('Hello TypeScript!');
 logMessage3('Hello TypeScript!');
 logMessage4('Hello TypeScript!');
+
+isUserSignedIn('ABC', 'Kumahack');
+isUserSignedIn('DEF');
+isUserSignedIn2('ABC');
