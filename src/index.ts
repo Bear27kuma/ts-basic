ab9cf7e6cadbda2a6c17f258c7c2171d874e8548
Yuta Okuma <y.dream.nest@icloud.com>
2022-03-11 21:30:00 +0900

    レストパラメーター

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@
 
 // 04.関数の型定義
 import { logMessage, logMessage2, logMessage3, logMessage4 } from './function/basic';
-import { isUserSignedIn, isUserSignedIn2 } from './function/parameters';
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters';
 
 logMessage('Hello TypeScript!');
 logMessage2('Hello TypeScript!');
@@ -26,3 +26,5 @@ logMessage4('Hello TypeScript!');
 isUserSignedIn('ABC', 'Kumahack');
 isUserSignedIn('DEF');
 isUserSignedIn2('ABC');
+const sum = sumProductsPrice(100, 200, 300, 400, 500);
+console.log('Function parameters sample 5:', sum);
