7e0d3d883006c92b1bd66addc3ef185e8fc943b2
Yuta Okuma <y.dream.nest@icloud.com>
2022-03-12 19:14:18 +0900

    インデックスシグネチャ

--- src/object/object.ts
+++ src/object/object.ts
@@ -39,4 +39,17 @@ export default function objectSample() {
   // kumahack.firstName = 'Tanjiro';
 
   console.log('Object object sample 3:', kumahack);
+
+  // インデックスシグネチャ
+  const capitals: {
+    [countryName: string]: string;
+  } = {
+    Japan: 'Tokyo',
+    Korea: 'Seoul',
+  };
+
+  capitals.China = 'Beijing';
+  capitals.Canada = 'Ottawa';
+
+  console.log('Object object sample 4:', capitals);
 }
