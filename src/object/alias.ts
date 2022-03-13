8c4dba72a0c2905ffe1ec18e7269e680ebd21376
Yuta Okuma <y.dream.nest@icloud.com>
2022-03-13 13:59:10 +0900

    合併型（union）と交差型（intersection）

--- src/object/alias.ts
+++ src/object/alias.ts
@@ -19,4 +19,56 @@ export default function typeAliasSample() {
     name: 'United States of America',
   };
   console.log('Object alias sample 2:', america);
+
+  // 合併型（union）と交差型（intersection）
+  type Knight = {
+    hp: number;
+    sp: number;
+    weapon: string;
+    swordSkill: string;
+  };
+
+  type Wizard = {
+    hp: number;
+    mp: number;
+    weapon: string;
+    magicSkill: string;
+  };
+
+  // 合併型... KnightまたはWizardの型を持つ
+  type Adventure = Knight | Wizard;
+
+  // 交差型... KnightかつWizardの型を持つ
+  type Paladin = Knight & Wizard;
+
+  // Knight寄りの冒険者
+  const adventure1: Adventure = {
+    hp: 100,
+    sp: 30,
+    weapon: '木の剣',
+    swordSkill: '三連斬り',
+  };
+
+  // Wizard寄りの冒険者
+  const adventure2: Adventure = {
+    hp: 100,
+    mp: 30,
+    weapon: '木の杖',
+    magicSkill: 'ファイヤボール',
+  };
+
+  console.log('Object alias sample 3:', adventure1);
+  console.log('Object alias sample 4:', adventure2);
+
+  // Paladin（交差型）
+  const paladin: Paladin = {
+    hp: 300,
+    sp: 100,
+    mp: 100,
+    weapon: '銀の剣',
+    swordSkill: '三連斬り',
+    magicSkill: 'ファイヤボール',
+  };
+
+  console.log('Object alias sample 5:', paladin);
 }
